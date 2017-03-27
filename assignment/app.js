bd0f09493fbf0c75611bc3b0bd56b2da69c19da5
ravikiran <ravikiran3169@gmail.com>
2017-03-27 12:25:24 +0530

    modified assignment

--- assignment/app.js
+++ assignment/app.js
@@ -8,24 +8,21 @@ MSgcontroller.$inject=['$scope'];
 function MSgcontroller($scope) {
   var comma = ',';
 
-
-  var monthString = 'Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec';
-  $scope.name="empty";
+  $scope.name="";
 
 $scope.clickTrigger=function () {
   var findLength=$scope.name.split(',').length;
 console.log(findLength);
 
   if($scope.name == "")
-  $scope.name="Please enter data first!";
-else if (findLength <= 3) {
-
-  $scope.name="enjoy!";
-}
-else {
-      $scope.name="heavy!";
+  $scope.name="Empty!";
+  else if (findLength <= 3) {
+  $scope.name="Enjoy!";
   }
-
+  else {
+  $scope.name="Too much!";
+  }
+  
 }
 
 };
